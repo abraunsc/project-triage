1de4ad5f60149164e4fdb8154789b97814031719
abraunsc <abraunsc@iu.edu>
2019-09-18 12:56:52 -0400

    dvSz
    fbzdv z

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,14 +1,19 @@
-var waitingApp = new Vue({
-  el: '#patientWaitingApp',
+var patientRecordApp = new Vue({
+  el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
-      .then( response => response.json() )
-      .then( json => {waitingApp.patients = json})
-      ;
+      .then(response => response.json())
+      .then(json => {patientRecordApp.patients = json});
 
       // Means the same at this
       // fetch('https://randomuser.me/api/')
@@ -20,33 +25,5 @@ var waitingApp = new Vue({
   created() {
     this.fetchPatients();
   }
+
 })
-
-
-
-
-
-
-// var waitingApp = new Vue({
-//   el: '#patientWaitingApp',
-//   data: {
-//     patients: []
-//   },
-//   methods: {
-//     fetchPatients() {
-//       fetch('dummy.php')
-//       .then(response => response.json())
-//       .then(json => {waitingApp.patients = json});
-//
-//       // Means the same at this
-//       // fetch('https://randomuser.me/api/')
-//       // .then(function(response) {return response.json()})
-//       // .then(function(json) {waitingApp.people = json});
-//
-//     }
-//   },
-//   created: function() {
-//     this.fetchPatients();
-//   }
-//
-// })
