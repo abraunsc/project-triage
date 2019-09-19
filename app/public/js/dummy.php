45f5236e72e0979a7b4a672a9e9e6c04208dd630
abraunsc <abraunsc@iu.edu>
2019-09-19 10:29:02 -0400

    php

---
+++ app/public/js/dummy.php
@@ -0,0 +1,59 @@
+[
+  {
+    "patientGuid": "1011",
+    "firstName": "Meghan",
+    "lastName": "Markle",
+    "dob": "1996-10-12",
+    "sexAtBirth": "F",
+    "patientVisit": [
+    {
+    	"visitId": "1",
+    	"visitDescription": "Flu",
+    	"visitDateUtc": "2019-07-07",
+    	"patientVisitWaiting": [ <!--should there be brackets here? -->
+    	{
+    		"priority": "high"
+    	}
+    	]
+    }
+    ]
+  },
+  {
+    "patientGuid": "100",
+    "firstName": "Jose",
+    "lastName": "Wells",
+    "dob": "1999-10-1",
+    "sexAtBirth": "M",
+    "patientVisit": [
+    {
+    	"visitId": "102",
+    	"visitDescription": "Ebola",
+    	"visitDateUtc": "2019-05-11",
+    	"patientVisitWaiting": [  <!--should there be brackets here? -->
+    	{
+    		"priority": "high"
+    	}
+    	]
+    }
+    ]
+  },
+  {
+    "patientGuid": "7414",
+    "firstName": "Lavender",
+    "lastName": "Gooms",
+    "dob": "1988-2-29",
+    "sexAtBirth": "M",
+    "patientVisit": [
+    {
+    	"visitId": "301",
+    	"visitDescription": "Dysentery",
+    	"visitDateUtc": "2019-07-11",
+    	"patientVisitWaiting": [  <!--should there be brackets here? -->
+    	{
+    		"priority": "medium"
+    	}
+    	]
+    }
+    ],
+  }
+]
